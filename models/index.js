2a6712770be82aced99cf9d415f838e76dcad1f7
Nate <itsnzte@gmail.com>
2022-09-07 13:09:21 -0700

    started associations, created links for Post and User model

--- models/index.js
+++ models/index.js
@@ -3,3 +3,23 @@ const Post = require("./post");
 const User = require("./user");
 
 module.exports = { User, Post, Comment };
+
+User.hasMany(Post, {
+  foreignKey: "username",
+  onDelete: "CASCADE"
+});
+
+User.hasMany(Comment, {
+  foreignKey: "username",
+  onDelete: "CASCADE"
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "username",
+  onDelete: "CASCADE"
+});
+
+Post.belongsTo(User, {
+  foreignKey: "username",
+  onDelete: "CASCADE"
+});
