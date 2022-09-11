d8b3c8a69235a451d6859b8e8bb7d3684ef55947
Nate <itsnzte@gmail.com>
2022-09-11 00:55:23 -0700

    created a success page after the user has successfully created a new account

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -74,7 +74,7 @@ router.post("/register", async (req, res) => {
       
     });
 
-    res.status(200).json({message: "Successfully Created User!",newUser});
+    res.render("successful");
 
   } catch (err) {
     res.status(500).json({ message: "Server Error, cannot create user..." });
--- controllers/signUpRoutes.js
+++ controllers/signUpRoutes.js
@@ -9,4 +9,13 @@ router.get("/", async (req, res) => {
   }
 });
 
+// successfully signed in 
+router.get("/success", async (req, res) => {
+  try {
+    res.render("successful");
+  } catch (err) {
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
 module.exports = router;
