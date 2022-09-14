92c338cee399effc2474faf1eb021094aa6f5255
Nate <itsnzte@gmail.com>
2022-09-14 01:23:46 -0700

    trying to get right styles when user clicks on a post

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,19 @@
 // Core
 
 const postsContainer = document.querySelector(".home-container-posts");
+const commentContainer = document.querySelector(".post-container-comments");
 
 postsContainer.addEventListener("click", (e) => {
+  let w = window.innerWidth;
+
+  if (w >= 980) {
+    postsContainer.style.width = "60%";
+    commentContainer.style.display = "block";
+    e.target.style.cssText = "border: 1px solid var(--light-clr)";
+    e.target.style.width = "91%";
+    // e.target.style.border = "1px solid var(--light-clr)";
+
+  }
   let post = e.target;
   console.log(post);
 });
