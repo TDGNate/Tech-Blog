e5636a8d2186f1581b4e3a9b0593e578dc13df45
Nate <itsnzte@gmail.com>
2022-09-14 02:23:23 -0700

    added effects and sticky posts for desktop on the home page

--- public/js/index.js
+++ public/js/index.js
@@ -4,7 +4,11 @@ const postsContainer = document.querySelector(".home-container-posts");
 const commentContainer = document.querySelector(".post-container-comments");
 
 postsContainer.addEventListener("click", (e) => {
+
+  removePicked();
+
   let w = window.innerWidth;
+  let post = e.target;
 
   if (e.target.className == "home-container-posts") {
     return;
@@ -13,10 +17,34 @@ postsContainer.addEventListener("click", (e) => {
   if (w >= 980) {
     postsContainer.style.width = "60%";
     commentContainer.style.display = "block";
-    e.target.style.cssText = "border: 1px solid var(--light-clr)";
-    e.target.style.width = "91%";
+    post.style.cssText = "border: 1px solid var(--light-clr)";
+    post.style.width = "91%";
+
+    post.classList.add("picked");
+
+    // Getting Clicked Post Content for later 
+    let elTitle = post.querySelector(".post-title").textContent;
+    let elContent = post.querySelector(".post-content").textContent;
+    let elUser = post.querySelector(".post-user").textContent;
+    let elCreated = post.querySelector(".post-created").textContent;
+    
+    console.log(elTitle, elContent, elUser, elCreated);
 
   }
-  let post = e.target;
+
   console.log(post);
 });
+
+function removePicked() {
+  let posts = document.querySelectorAll(".post");
+
+  for (i = 0; i < posts.length; i++) {
+    let post = posts[i];
+
+    if (post.classList.contains("picked")) {
+      post.classList.remove("picked");
+      post.style.cssText = "border: none";
+      post.style.width = "45%";
+    }
+  }
+}
