ac4a75ce1ac13e2dfc3a64945a592b31eaa138db
Nate <itsnzte@gmail.com>
2022-09-14 01:33:58 -0700

    user can no longer click the background to outline border

--- public/js/index.js
+++ public/js/index.js
@@ -6,12 +6,15 @@ const commentContainer = document.querySelector(".post-container-comments");
 postsContainer.addEventListener("click", (e) => {
   let w = window.innerWidth;
 
+  if (e.target.className == "home-container-posts") {
+    return;
+  }
+
   if (w >= 980) {
     postsContainer.style.width = "60%";
     commentContainer.style.display = "block";
     e.target.style.cssText = "border: 1px solid var(--light-clr)";
     e.target.style.width = "91%";
-    // e.target.style.border = "1px solid var(--light-clr)";
 
   }
   let post = e.target;
