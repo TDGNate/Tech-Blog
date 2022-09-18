da51129dff99bb63c5a2aaa8312eb2f3e4c6cbcd
Nate <itsnzte@gmail.com>
2022-09-18 12:36:30 -0700

    retrieved post id and user id and user's message to complete the call

--- public/js/posts_comments/add_comments/desktop.js
+++ public/js/posts_comments/add_comments/desktop.js
@@ -1,1 +1,34 @@
 console.log("link");
+
+const sendBtn = document.getElementById("deskCommentBtn");
+
+if (sendBtn) {
+
+  sendBtn.addEventListener("click", async (e) => {
+    e.preventDefault();
+
+    // get User's Comment 
+    const comment = document.getElementById("deskComment").value;
+
+    // check if the input field is empty 
+    if (comment == "") {
+
+      swal({
+        title: "Hold up...",
+        text: "Can't send anything if there's isn't anything to send silly",
+        button: "Ok"
+      });
+
+      return;
+    }
+
+    // get the value ID from the picked post, so we know which post to send the comment 
+    const postId = parseInt(document.querySelector(".picked").getAttribute("value"));
+
+    // Get the User's ID to complete the API call along with the post ID 
+    const userId = parseInt(document.querySelector(".home-hello-user").getAttribute("value"));
+
+    console.log(comment, postId, userId);
+  });
+
+}
