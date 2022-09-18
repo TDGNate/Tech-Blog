2dd130aad97e701233115e1f78c0c414dd12121d
Nate <itsnzte@gmail.com>
2022-09-18 14:36:31 -0700

    fixed up my desktop js file, and added where user's can click enter to submit their comment

--- public/js/posts_comments/add_comments/desktop.js
+++ public/js/posts_comments/add_comments/desktop.js
@@ -1,36 +1,54 @@
-console.log("link");
+// Desktop Version for Sending Comments 
 
 const sendBtn = document.getElementById("deskCommentBtn");
 
+// For no console log errors 
 if (sendBtn) {
 
-  sendBtn.addEventListener("click", async (e) => {
-    e.preventDefault();
+   sendBtn.addEventListener("click", initDeskComments);
 
-    // get User's Comment 
-    const comment = document.getElementById("deskComment").value;
+}
 
-    // check if the input field is empty 
-    if (comment == "") {
+const textArea = document.getElementById("deskComment");
 
-      swal({
-        title: "Hold up...",
-        text: "Can't send anything if there's isn't anything to send silly",
-        button: "Ok"
-      });
+if (textArea) {
 
-      return;
+  textArea.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        
+      initDeskComments();
+
     }
+});
 
-    // get the value ID from the picked post, so we know which post to send the comment 
-    const postId = parseInt(document.querySelector(".picked").getAttribute("value"));
+}
 
-    // Get the User's ID to complete the API call along with the post ID 
-    const userId = parseInt(document.querySelector(".home-hello-user").getAttribute("value"));
+// Setting up the add comments functionality 
+function initDeskComments() {
 
-    deskAddComment(comment, userId, postId);
+  // get User's Comment 
+  const comment = document.getElementById("deskComment").value;
 
-    document.getElementById("deskComment").value = "";
-  });
+  // check if the input field is empty 
+  if (comment == "") {
+
+    swal({
+      title: "Hold up...",
+      text: "Can't send anything if there's isn't anything to send silly",
+      button: "Ok"
+    });
+
+    return;
+  }
+
+  // get the value ID from the picked post, so we know which post to send the comment 
+  const postId = parseInt(document.querySelector(".picked").getAttribute("value"));
+
+  // Get the User's ID to complete the API call along with the post ID 
+  const userId = parseInt(document.querySelector(".home-hello-user").getAttribute("value"));
+
+  deskAddComment(comment, userId, postId);
+
+  document.getElementById("deskComment").value = "";
 
 }
